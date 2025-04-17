8783967eb88127322fcb42787553b5e1d3cfa909
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2025-04-17 22:48:20 +0530

    added background worker and style

--- server.py
+++ server.py
@@ -1,12 +1,30 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
 import json
 from db import add_message, get_all_messages
-
-app = FastAPI()
+import asyncio
+from contextlib import asynccontextmanager
 
 connected_clients = set()
 messages = []  # in-memory cache
+import time 
+async def clear_messages():
+    global messages
+    while True:
+        messages = [] 
+        await asyncio.sleep(3000)
+
+@asynccontextmanager
+async def startup_event(app: FastAPI):
+    asyncio.create_task(clear_messages())
+    yield
+
+app = FastAPI(lifespan=startup_event)
+
+
+@app.get("/")
+def serve():
+    return FileResponse("index.html")
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
